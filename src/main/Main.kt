e25561de5ca761d111732baf767ecf0c7e090e86
willmo3 <thelordofbouge@gmail.com>
2024-06-25 16:27:07 -0400

    Add argument file requirements

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,11 +1,23 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.types.file
 
 class TlaRobustness : CliktCommand() {
-    private val sysPath by argument(name="[system model path]", help="tla+ model of the system")
-    private val sysConfig by argument(name="[system config path]", help="tla+ configuration file for the system")
-    private val envPath by argument(name="[environment path]", help="tla+ model of the environment")
-    private val envConfig by argument(name="[environment config path]", help = "tla+ configuration file for environment")
+
+
+    // ***** ARGUMENTS ***** //
+
+    private val sysPath
+        by argument(name="[system model path]", help="tla+ model of the system").file(mustExist = true)
+    private val sysConfig 
+        by argument(name="[system config path]", help="tla+ configuration file for the system").file(mustExist = true)
+    private val envPath
+        by argument(name="[env path]", help="tla+ model of the environment").file(mustExist = true)
+    private val envConfig
+        by argument(name="[env config path]", help = "tla+ configuration file for environment").file(mustExist = true)
+
+
+    // ***** MAIN ***** //
 
     override fun run() {
         println("Tla Robustness")
